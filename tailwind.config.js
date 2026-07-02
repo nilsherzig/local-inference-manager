de96ab7e49403bb1ac32cd39e261f1e637a03b72
nilsherzig <git@nilsherzig.com>
2026-07-02 19:07:40 +0200

    there used to be waay to much blue
    Signed-off-by: nilsherzig <git@nilsherzig.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./internal/web/templates/**/*.gohtml"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        // Muted, dusty purple used as the single UI accent.
+        mauve: {
+          200: "#ddd0e6",
+          300: "#c8b6d6",
+          400: "#ab90c1",
+          500: "#8d6fa4",
+          600: "#735988",
+        },
+      },
+    },
+  },
   plugins: [],
 };
